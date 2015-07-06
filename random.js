c237743007b1064316a5f6c220278c2c23efe9ce
miguelangel9 <almiguel@live.com.mx>
2015-07-05 20:45:48 -0500

    Versión 2

--- random.js
+++ random.js
@@ -1,14 +1,15 @@
-// Math.random() devuelve numero aleatorio entre 0 y 1.
+/ Math.random() devuelve numero aleatorio entre 0 y 1.
 var numero = Math.random();
+var str =' MENOR que 0.66'
 if (numero <= 0.33)
   {
-   console.log('\n' + numero + ' MENOR que 0.33 \n');
+   console.log('\n' + numero + str + '\n');
   }
 else if (numero <= 0.66)
   {
-   console.log('\n' + numero + ' MENOR que 0.66 \n');
+   console.log('\n' + numero + str + '\n');
   }
 else
-  {
-   console.log('\n' + numero + ' MAYOR que 0.66 \n');
+  {str =' MAYOR que 0.66'
+   console.log('\n' + numero + str +'\n');
   }
